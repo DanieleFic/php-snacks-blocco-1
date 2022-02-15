0fce1af5572ddf29c5864731536e1eedb5d437f9
DanieleFic <reb_ficus22@hotmail.it>
2022-02-15 16:08:10 +0100

    fatto C

---
+++ snack-C.php
@@ -0,0 +1,30 @@
+<!-- Snack C
+Creare un array con 15 numeri casuali, tenendo conto che 
+l’array non dovrà contenere lo stesso numero più di una volta -->
+
+
+
+<?php 
+
+$arrayNumeri = [];
+
+
+$i = 0;
+while (++$i <= 15) {
+    $numberoRandom = rand(0, 100);
+    /* echo "entrato in while\n".'<br>'; */
+    /* echo "numero random nel while:".$numberoRandom; */
+    /* echo $arrayNumeri; */
+    if(!in_array($numberoRandom, $arrayNumeri )){
+        /* echo "entrato in if \n".'<br>'; */
+        $arrayNumeri[] =  $numberoRandom;
+        /* echo "numero random nell if:".$numberoRandom;
+        echo '-'.$i.$arrayNumeri.$numeroRandom.'<br>';  */
+    }else{
+        --$i;
+    }
+    
+    var_dump ($arrayNumeri); 
+}
+
+?>
